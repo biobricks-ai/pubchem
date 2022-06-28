db63f94560f80048429de1365ca23accd1b2ad83
Jose A. Jaramillo-Villegas <jaljavi@icloud.com>
2022-06-28 08:43:16 -0400

    verbose

--- R/buildCompoundsSDF.R
+++ R/buildCompoundsSDF.R
@@ -15,10 +15,14 @@ print("Reading SDF files from /raw")
 deps <- dir_ls("raw")
 out <- dir_create("data")
 
+print(deps[1])
+start_time <- Sys.time()
 mdb <- read_sdf(deps[1], coord = FALSE)
 props <- mdb_get_prop_names(mdb)
 props <- props[props != ""]
 df <- get_props(mdb, props)
+stop_time <- Sys.time()
+print(stop_time - start_time)
 
 deps <- deps[-1]
 
@@ -33,4 +37,4 @@ for (file in deps) {
 }
 
 print("Writing parquet file to /data")
-write_parquet(df, "data/Compunds.parquet")
+write_parquet(df, "data/Compounds.parquet")
