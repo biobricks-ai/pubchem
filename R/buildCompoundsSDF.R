1c50cd8a16de9f6e437e4e1b0f714955f636f216
Jose A. Jaramillo-Villegas <jaljavi@icloud.com>
2022-06-28 08:39:32 -0400

    build parquet from SDF files

--- R/buildCompoundsSDF.R
+++ R/buildCompoundsSDF.R
@@ -10,72 +10,27 @@ library(methods)
 library(R.utils)
 library(cinf)
 
-start_time <- Sys.time()
-mdb <- read_sdf("data/Compound_012000001_012500000.sdf", coord = FALSE)
-stop_time <- Sys.time()
-print("read_sdf")
-print(stop_time - start_time)
+print("Reading SDF files from /raw")
 
+deps <- dir_ls("raw")
+out <- dir_create("data")
+
+mdb <- read_sdf(deps[1], coord = FALSE)
 props <- mdb_get_prop_names(mdb)
 props <- props[props != ""]
-start_time <- Sys.time()
-df1 <- get_props(mdb, props)
-print("get_props")
-stop_time <- Sys.time()
-print(stop_time - start_time)
-nrow(df1)
+df <- get_props(mdb, props)
 
-start_time <- Sys.time()
-mdb2 <- read_sdf("data/Compound_012500001_013000000.sdf", coord = FALSE)
-stop_time <- Sys.time()
-print("read_sdf")
-print(stop_time - start_time)
+deps <- deps[-1]
 
-start_time <- Sys.time()
-df2 <- get_props(mdb2, props)
-df <- bind_rows(df1, df2)
-stop_time <- Sys.time()
-print("bind_rows get_props")
-print(stop_time - start_time)
-print(length(mdb))
-nrow(df)
+for (file in deps) {
+  print(file)
+  start_time <- Sys.time()
+  mdb <- read_sdf(file, coord = FALSE)
+  dft <- get_props(mdb, props)
+  df <- bind_rows(df, dft)
+  stop_time <- Sys.time()
+  print(stop_time - start_time)
+}
 
-
-# tsub <- \(p,r){partial(gsub,pattern=p,replacement=r,ignore.case=T)}
-# deps <- dir_ls("download")
-# out  <- dir_create("data")
-# outs <- path(out, path_file(deps)) |> tsub("\\..*", ".xml")()
-# # fs::dir_ls(out) |> fs::file_delete()
-# # walk2(deps, outs, ~ gunzip(.x, .y, remove = FALSE))
-
-# files <- list.files(path = "data", full.names = TRUE)
-# print(files[[1]])
-# df1 <- readLines(files[1], n = 1000)
-# df <- data.frame(values = df1)
-# # df1 <- read_xml(files[[1]])
-# # df1 <- xmlToDataFrame(files[[1]])
-# print(df, max = 1000)
-
-
-
-
-
-
-# df <- list.files(path = "data", full.names = TRUE) %>%
-#   lapply(xmlToDataFrame) %>%
-#   bind_rows
-
-# df <- xmlToDataFrame()
-
-
-
-
-# save_parquet <- function(file) {
-#   path <- fs::path_ext_remove(file) |> fs::path_ext_set("parquet") |>
-#     fs::path_file()
-#   df   <- vroom::vroom(file, comment = "#", delim = ",", )
-#   arrow::write_parquet(df, fs::path(outdir, path))
-# }
-
-
-# fs::dir_ls("data", regexp = "(*.gz)?$") |> walk(save_parquet)
+print("Writing parquet file to /data")
+write_parquet(df, "data/Compunds.parquet")
