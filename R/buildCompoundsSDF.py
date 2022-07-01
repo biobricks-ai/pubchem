cf6b8c8d1debf617af963455de782e64308da71d
Jose A. Jaramillo-Villegas <jaljavi@icloud.com>
2022-07-01 14:53:42 -0400

    Ready to run build

--- R/buildCompoundsSDF.py
+++ R/buildCompoundsSDF.py
@@ -1,29 +1,52 @@
-from tkinter.tix import FileSelectBox
 from rdkit.Chem import PandasTools
 import pandas as pd
 import pyarrow as pyarrow
 import fastparquet as fastparquet
 import os
+import time
 
-files = os.listdir("./raw")
-files.sort()
-# print(len(files))
-# print(files[100])
-files = files[0:3]
-# print(len(files))
+def FmtMem(num):
+    for unit in ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f} {unit}"
+        num /= 1024.0
+    return f"{num:.1f} YiB"
 
-filename = "raw/" + files[0]
-print(filename)
-DF = PandasTools.LoadSDF(filename, molColName=None)
-print(len(DF))
-files = files[1:]
-print(len(files))
+os.mkdir("data")
+FilesTotal = os.listdir("./raw")
+FilesTotal.sort()
 
-for file in files:
-    filename = "raw/" + file
-    print(filename)
-    DFT = PandasTools.LoadSDF(filename, molColName=None)
-    DF = pd.concat([DF, DFT], ignore_index=True)
-    DF.info()
+NumFiles = len(FilesTotal)
+print("Total files:", NumFiles)
+NumFilesPerParquet = 40
+print("Number of files per parquet:", NumFilesPerParquet)
 
-DF.to_parquet('data/PubChemCompounds_000_099.parquet')
+for i in range(0, NumFiles//NumFilesPerParquet + 1):
+
+    Files = FilesTotal[:NumFilesPerParquet]
+    FilesTotal = FilesTotal[NumFilesPerParquet:]
+    print("")
+    print("File, Time, Molecules, Memory")
+    Filename = "raw/" + Files[0]
+    Tic = time.time()
+    DF = PandasTools.LoadSDF(Filename, molColName=None)
+    Mem = DF.memory_usage(deep=True).sum()
+    Toc = time.time() - Tic
+    print(Files[0], f"{Toc:.1f} s", len(DF.index), FmtMem(Mem))
+    Files = Files[1:]
+
+    for File in Files:
+        Tic = time.time()
+        Filename = "raw/" + File
+        DFT = PandasTools.LoadSDF(Filename, molColName=None)
+        DF = pd.concat([DF, DFT], ignore_index=True)
+        Mem = DF.memory_usage(deep=True).sum()
+        Toc = time.time() - Tic
+        print(File, f"{Toc:.1f} s", len(DF.index), FmtMem(Mem))
+
+    ParquetFile = "data/Compound_" + f"{i}" + ".parquet"
+    print(ParquetFile)
+    DF.to_parquet(ParquetFile)
+    ParquetFile = ParquetFile + ".gzip"
+    print(ParquetFile)
+    DF.to_parquet(ParquetFile, compression='gzip')
