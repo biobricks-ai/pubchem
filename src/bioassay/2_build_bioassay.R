d45fb1c7942887c800ae9781045b04f937f942e3
insilica <tom@insilica.co>
2023-04-05 19:52:57 -0400

    first version of bioassay complete

--- src/bioassay/2_build_bioassay.R
+++ src/bioassay/2_build_bioassay.R
@@ -1,71 +1,82 @@
 # Extract every downloaded bioassay csv.gz into parquet tables. 
 # README: https://ftp.ncbi.nlm.nih.gov/pubchem/Bioassay/CSV/README 
 # parsing done with respect to README.
-library(dplyr)
+pacman::p_load(future, furrr)
+future::plan(future::multicore(workers=30))
 
-outdir <- fs::dir_create("brick/bioassay/csv.parquet")
-stgdir <- tempdir()
+stgdir <- fs::dir_create("staging/bioassay")
 
-process_zip <- function(F){
-  td <- tempdir()
-  unzip(F,exdir=td)
-  fs::dir_ls(td,recurse=TRUE,glob="*.csv.gz")
-}
+# UNZIP ALL FILES ============================================================================
+zipfs <- fs::dir_ls("download/bioassay/csv/",glob="*.zip")
+furrr::future_walk(zipfs, ~ unzip(.,exdir=stgdir), .progress=TRUE)
 
-required_header <- c("PUBCHEM_RESULT_TAG","PUBCHEM_SID","PUBCHEM_CID",
-"PUBCHEM_ACTIVITY_OUTCOME","PUBCHEM_ACTIVITY_SCORE","PUBCHEM_ACTIVITY_URL",
-"PUBCHEM_ASSAYDATA_COMMENT")
+# CREATE BRICK/BIOASSAY.PARQUET ==============================================================
 
-header_row_indicator <- c("RESULT_TYPE","RESULT_DESCR",
-"RESULT_UNIT","RESULT_IS_ACTIVE_CONCENTRATION",
-"RESULT_IS_ACTIVE_CONCENTRATION_QUALIFIER",
-"RESULT_ATTR_CONC_MICROMOL")
+required_header <- c("PUBCHEM_RESULT_TAG","PUBCHEM_SID","PUBCHEM_CID","PUBCHEM_ACTIVITY_OUTCOME","PUBCHEM_ACTIVITY_SCORE","PUBCHEM_ACTIVITY_URL","PUBCHEM_ASSAYDATA_COMMENT")
+header_row_valu <- c("RESULT_TYPE","RESULT_DESCR","RESULT_UNIT","RESULT_IS_ACTIVE_CONCENTRATION","RESULT_IS_ACTIVE_CONCENTRATION_QUALIFIER","RESULT_ATTR_CONC_MICROMOL")
 
-allcols <- c()
-stage_parquet = function(csvgz){
-  # read csv.gz file
-  df <- readr::read_csv(csvgz, col_types = readr::cols())
-  
-  # remove extra header rows
-  rtag <- df$PUBCHEM_RESULT_TAG
-  last_hrow <- rtag |> purrr::detect_index(~ !(. %in% header_row_indicator))
-  df <- df[last_hrow:nrow(df),]
+# write 2 million rows at a time to parquet
+# @param csvs list of csv.gz files
+stage_parquet = function(csvs,aids,chunk=1e6){
+  bigdf <- data.frame()
+  for(i in 1:length(csvs)){
+    print(sprintf("%s out of %s nr %s",i,length(csvs),nrow(bigdf)))
+    
+    # 1:7 are the pubchem required columns
+    df <- data.table::fread(csvs[i])[,1:8] 
+    df$aid <- as.integer(aids[i])
 
-  # mutate all non-required headers to character type
-  df <- df |> mutate(across(!one_of(required_header), as.character))
-
-  # update allcols
-  allcols <<- c(allcols, colnames(df)) |> unique()
-
-  # write to staging directory
-  ppath <- stgdir |> fs::path(uuid::UUIDgenerate(),ext="parquet")
-  arrow::write_parquet(df, ppath)
-  ppath
-}
-
-# TODO replace files rather than deleting everything
-fs::dir_ls(outdir) |> fs::file_delete()
-
-zipfl <- fs::dir_ls("download/bioassay/csv/",glob="*.zip")
-csvgz <- zipfl |> purrr::map(process_zip,.progress=TRUE) |> purrr::flatten_chr()
-staged_parquet <- csvgz |> purrr::map_chr(stage_parquet,.progress=TRUE)
-
-# CREATE OUTPUT PARQUET DIRECTORY ============================================================
-# read the staged parquet files, add missing columns, add to bigDF, write 1e7 sized chunks 
-bigDF <- arrow::read_parquet(staged_parquet[1]) |> collect() 
-
-# add missing columns to bigdf
-missing_cols <- setdiff(allcols, names(df))
-newcols <- data.frame(matrix(NA_character_, nrow = nrow(bigdf), ncol = length(missing_cols)))
-colnames(newcols) <- missing_cols
-bigDF <- bigDF |> bind_cols(newcols)
-
-for(i in 2:length(staged_parquet)){
-  df <- arrow::read_parquet(staged_parquet[i]) |> collect()
-  bigDF <- dplyr::bind_rows(bigDF,df)
-  while(nrow(bigDF) > 1e7){
-    outDF <- bigDF[1:1e7,]
-    bigDF <- bigDF[-(1:1e7),]
-    arrow::write_parquet(outDF, outname)
+    # remove header rows
+    isheader <- sapply(df[1:10,1], \(x){ x %in% header_row_valu})
+    hrow <- which(!isheader)[1]
+    df <- df[hrow:nrow(df),]
+    
+    bigdf <- data.table::rbindlist(list(bigdf, df))
+    while(nrow(bigdf) > 0 && (nrow(bigdf) > chunk || i == length(csvs))){
+      wrtdf <- bigdf[1:min(nrow(bigdf),chunk),]
+      data.table::setcolorder(wrtdf,c("aid",required_header))
+      
+      width <- 10
+      minaid <- sprintf("%0*d", width, min(wrtdf$aid))
+      maxaid <- sprintf("%0*d", width, max(wrtdf$aid))
+      fname <- sprintf("%s-%s.parquet",minaid,maxaid)
+      outp <- fs::path("brick/bioassay.parquet",fname)
+      arrow::write_parquet(wrtdf,outp)
+      
+      bigdf <- if(nrow(bigdf) > chunk){ bigdf[-(1:chunk),] }else{ data.frame() }
+    }
   }
 }
+
+# sort csv.gz by assay id
+# chunk csv.gz files into chunks of size 1e4
+csvgz <- fs::dir_ls(stgdir,recurse=T,glob="*.csv.gz") 
+csvid <- as.numeric(gsub(".csv.gz","",fs::path_file(csvgz))) 
+
+said <- split(csvid[order(csvid)], ceiling(seq_along(csvid)/1e4))
+scsv <- split(csvgz[order(csvid)], ceiling(seq_along(csvgz)/1e4))
+fs::dir_create("brick/bioassay.parquet")
+furrr::future_walk2(scsv, said, stage_parquet, .progress=TRUE)
+
+# PROGRESS TRACKER ===========================================================================
+# 304 million bioactivities today means ~300 files
+if(interactive()){
+  df <- fs::dir_ls("brick/bioassay.parquet") |> fs::file_info()
+
+  mintime <- min(df$modification_time)
+  maxtime <- max(df$modification_time)
+  diffsec <- as.numeric(maxtime - mintime, units="hours")
+  avgdur  <- diffsec/nrow(df)
+  # calculate remaining time assuming 300 files
+  eta <- avgdur * (300 - nrow(df))
+  eta <- as.difftime(eta, units="hours")
+  print(eta)
+  print(nrow(df),"files created")
+}
+
+# SIMPLE TEST ================================================================================
+df <- arrow::open_dataset("brick/bioassay.parquet") |> dplyr::count(aid) |> dplyr::collect()
+assertthat::assert_that(length(setdiff(csvid, df$aid)) == 0)
+
+# CLEAN UP ===================================================================================
+fs::dir_delete(stgdir)
